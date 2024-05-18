4b79a4679ceff60de1a305b54d9866dfa8f7be14
Pulkit <pulkituppal95@gmail.com>
2024-05-19 00:02:27 +0530

    updated url in constans file

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:4173",
+    "https://chat-nexus-2hmfglmvt-pulkit-1110s-projects.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
