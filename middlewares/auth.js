f635a6284a075d2aa3f520a24b191087f7aed7d3
Pulkit <pulkituppal95@gmail.com>
2024-05-19 01:19:12 +0530

    Updated Socket Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -46,8 +46,7 @@ const socketAuthenticator = async (err, socket, next) => {
 
     const user = await User.findById(decodedData._id);
 
-    if (!user)
-      return next(new ErrorHandler("Please login to access this route", 401));
+    if (!user) return next(new ErrorHandler("User not found", 401));
 
     socket.user = user;
 
