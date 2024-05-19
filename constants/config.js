eb5d5d279eade00b47a4778e2c74eec0ece8508a
Pulkit <pulkituppal95@gmail.com>
2024-05-20 01:21:45 +0530

    Added Frontend URL

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://chat-nexus-wine.vercel.app",
-    "https://chat-nexus-lq94c6syo-pulkit-1110s-projects.vercel.app",
+    "https://main--luxury-boba-dbbabf.netlify.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
